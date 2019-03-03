a7d1f6a658439ba3ed20ab6cd25a4f0c13daa1f0
github username <github email address>
2019-03-03 04:06:07 -0500

    not finished, submitted mistake report for map

--- index.js
+++ index.js
@@ -4,16 +4,32 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+        // debugger
+        for(let i = 0; i < newCollection.length; i++){
+          callback(newCollection[i])
+        }
+      return collection
 
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+      const newArray = [];
+      for(let i = 0; i < newCollection.length; i++){
+        newArray.push(callback(newCollection[i]));
+      }
+      return newArray;
     },
 
-    reduce: function() {
+    reduce: function(collection, callback, acc=0) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
+      for(let i = 0; i < newCollection.length; i++){
+        acc = callback(acc, newCollection[i], newCollection)
+      }
+      return acc;
     },
     
     functions: function() {
